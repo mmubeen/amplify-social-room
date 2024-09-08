c734b2e8b9d3a56f777d72db2329d0993b566375
Reza Mubeen <mrmsconsult@gmail.com>
2024-09-08 16:59:48 -0500

    fix type declaration

--- amplify/data/publishCursor.ts
+++ amplify/data/publishCursor.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export const request = () => ({ })
 
-export const response = (ctx) => ctx.arguments
+export const response = (ctx:any) => ctx.arguments
